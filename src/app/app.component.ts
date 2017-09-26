5df587eb055e61019c155c317ff4e5854ad6b31f
pumanitro <pumanitro@gmail.com>
2017-09-26 18:02:28 +0200

    Trying out local storage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+
+  ngOnInit(){
+
+    // Check browser support
+    if (typeof(Storage) !== "undefined") {
+      // Store
+      localStorage.setItem("lastname", "Smith");
+      // Retrieve
+      console.log(localStorage.getItem("lastname"));
+    } else {
+      console.log('NOT SUPORTED');
+    }
+
+  }
 }
