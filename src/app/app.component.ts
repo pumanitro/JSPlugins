bed92346f12d4f511928413deea0eba475ba56ab
pumanitro <pumanitro@gmail.com>
2017-09-26 19:26:53 +0200

    Added possibility to run added js file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,38 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'app works!';
 
+  handleFiles(event){
+
+   let fileURL = URL.createObjectURL(event.srcElement.files[0]);
+
+    this.loadJS(fileURL, this.yourCodeToBeCalled, document.body);
+
+    this.yourCodeToBeCalled();
+
+  }
+
+  loadJS = function(url, implementationCode, location){
+    //url is URL of external file, implementationCode is the code
+    //to be called from the file, location is the location to
+    //insert the <script> element
+
+    var scriptTag:any = document.createElement('script');
+    scriptTag.src = url;
+
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+
+    location.appendChild(scriptTag);
+  };
+
+  yourCodeToBeCalled = function(){
+//your code goes here
+  };
+
+
   ngOnInit(){
 
-    // Check browser support
+   /* // Check browser support
     if (typeof(Storage) !== "undefined") {
       // Store
       localStorage.setItem("lastname", "Smith");
@@ -18,7 +47,7 @@ export class AppComponent implements OnInit{
       console.log(localStorage.getItem("lastname"));
     } else {
       console.log('NOT SUPORTED');
-    }
+    }*/
 
   }
 }
